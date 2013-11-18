156928a9caabb316b2913cbe1c8b28fdf3881f63
mikekavouras <mike@wantful.com>
2013-11-18 10:35:49 -0500

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harold::Application.config.secret_key_base = '05c7af73e40aedce49f10bd370b2a42d48e9127067254ace631e983a53f1917cdcfaa9457d5b831575bf0cafbd4e620ac32d4274bdd945ad644fafdfe8b036c3'
