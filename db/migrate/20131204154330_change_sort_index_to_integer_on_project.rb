857d2d73fb6aa1b36f32dd0d14b0268e18886036
Mike Kavouras <kavourasm@gmail.com>
2017-09-18 23:49:09 -0400

    specify version for migrations

--- db/migrate/20131204154330_change_sort_index_to_integer_on_project.rb
+++ db/migrate/20131204154330_change_sort_index_to_integer_on_project.rb
@@ -1,4 +1,4 @@
-class ChangeSortIndexToIntegerOnProject < ActiveRecord::Migration
+class ChangeSortIndexToIntegerOnProject < ActiveRecord::Migration[5.0]
   def change
     remove_column :projects, :sort_index
     add_column :projects, :sort_index, :integer
