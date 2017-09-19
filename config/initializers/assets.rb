c37852637e79fab3cce3885472eee03ed5e62a8d
Mike Kavouras <kavourasm@gmail.com>
2017-09-18 23:37:36 -0400

    1 line

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,1 @@
-Rails.application.config.assets.precompile += %w(
-  projects.css project.js login.css admin.css admin.js
-  directors_admin.js
-)
+Rails.application.config.assets.precompile += %w( projects.css project.js login.css admin.css admin.js directors_admin.js )
