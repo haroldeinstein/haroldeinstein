76439d1c3f92773b7f3adda22f4211bb597eed08
Mike Kavouras <mikekavouras@github.com>
2020-04-01 21:04:04 -0400

    Get the things showin

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,27 +15,25 @@ ActiveRecord::Schema.define(version: 20131204154330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "projects", force: :cascade do |t|
-    t.string   "title"
-    t.string   "video_url"
-    t.string   "vimeo_id"
-    t.string   "thumbnail_small"
-    t.string   "thumbnail_medium"
-    t.string   "thumbnail_large"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "sort_index"
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "video_url"
+    t.string "vimeo_id"
+    t.string "thumbnail_small"
+    t.string "thumbnail_medium"
+    t.string "thumbnail_large"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "sort_index"
+    t.index ["vimeo_id"], name: "index_projects_on_vimeo_id"
   end
 
-  add_index "projects", ["vimeo_id"], name: "index_projects_on_vimeo_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username"
   end
-
-  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
 end
